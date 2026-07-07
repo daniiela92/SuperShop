39889fccb33b7453874fcf6558ea4832dae81966
Daniela Pais <dannyelarbpais@hotmail.com>
2026-07-07 16:37:48 +0100

    SeedDb

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SuperShop.Data;
 
 namespace SuperShop
 {
@@ -29,6 +30,8 @@ namespace SuperShop
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddTransient<SeedDb>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
