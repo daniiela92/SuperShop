894968ab462c8c1c09788145d7999e84d996a4e5
Daniela Pais <dannyelarbpais@hotmail.com>
2026-07-10 17:51:39 +0100

    ImageHelper and ConverterHelper

---
+++ SuperShop/Helpers/IConverterHelper.cs
@@ -0,0 +1,14 @@
+﻿using SuperShop.Data.Entities;
+using SuperShop.Models;
+
+namespace SuperShop.Helpers
+{
+    public interface IConverterHelper
+    {
+
+        Product ToProduct(ProductViewModel model, string path,bool isNew);
+
+        ProductViewModel ToProductViewModel(Product product);
+
+    }
+}
