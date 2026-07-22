9a9bc535616765ed43518795ed58a97c5207f0c3
Daniela Pais <dannyelarbpais@hotmail.com>
2026-07-22 15:42:21 +0100

    Change user profile and password

---
+++ SuperShop/Models/ChangeUserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperShop.Models
+{
+    public class ChangeUserViewModel
+    {
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+    }
+}
